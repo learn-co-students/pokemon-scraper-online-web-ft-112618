435e8f96c7efbb97cfdf185f9c1ba0751b038bac
Henry <hanke.liu@gmail.com>
2019-01-09 19:34:33 -0500

    comments out bonus test

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,3 +1,4 @@
+require "pry"
 class Pokemon
 
   attr_accessor :id, :name, :type, :db
@@ -11,7 +12,14 @@ class Pokemon
 
 
   def self.save(pk_name, pk_type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES (pk_name, pk_type)")
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)",pk_name, pk_type)
+  end
+
+  def self.find(pk_id, db)
+    pokemon = db.execute("SELECT id, name, type FROM pokemon WHERE id = #{pk_id} ")
+    new_pokomen = Pokemon.new(id: pokemon[0][0], name: pokemon[0][1], type: pokemon[0][2], db: db)
+    #binding.pry
+
   end
 
 end
