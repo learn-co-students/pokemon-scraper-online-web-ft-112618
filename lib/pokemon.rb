6f2455d7c10e66c02520d0935d31de3bee436cf2
Nick Inman <nick@theoatmeal.com>
2019-01-10 03:26:14 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,27 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :hp, :db
+
+  def initialize(id: id, name: name, type: type, hp: hp, db: db)
+    @id = id
+    @name = name
+    @type = type
+    @hp = hp
+    @db = db
+  end
+
+    def self.save(name, type, db)
+      db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+      #binding.pry
+    end
+
+    def self.find(p_id, db)
+      poke = db.execute("SELECT * FROM pokemon WHERE id = ?", p_id).flatten
+      Pokemon.new(id: poke[0], name: poke[1], type: poke[2], hp:poke[3])
+    end
+
+    def alter_hp(new_hp, db)
+      health = db.execute("SELECT hp FROM pokemon WHERE id = ?", self.id).flatten.first 
+      damage = (health - new_hp) #gets HP from pokemon above and calculates damage, only needed if there was a change in HP
+      db.execute("UPDATE pokemon SET hp = (hp - ?) WHERE id = ?", damage, self.id)
+    end
 end
